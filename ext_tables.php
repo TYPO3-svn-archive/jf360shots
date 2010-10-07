7a491ac511e72c3eb3f0fe8396bbe3e1e2b657db
juergen.furrer <juergen.furrer@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-10-07 22:54:27 +0000

    Initial development
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/jf360shots/trunk@38953 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_tables.php
@@ -0,0 +1,31 @@
+<?php
+if (!defined ('TYPO3_MODE')) {
+	die ('Access denied.');
+}
+
+
+// Static
+t3lib_extMgm::addStaticFile($_EXTKEY, 'static/', '360 degrees shots');
+
+
+// tt_content
+t3lib_div::loadTCA('tt_content');
+$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1'] = 'layout,select_key,pages';
+$TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi1'] = 'pi_flexform';
+
+
+// ICON
+t3lib_extMgm::addPlugin(array(
+	'LLL:EXT:jf360shots/locallang_db.xml:tt_content.list_type_pi1',
+	$_EXTKEY . '_pi1',
+	t3lib_extMgm::extRelPath($_EXTKEY) . 'ext_icon.gif'
+),'list_type');
+
+t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:'.$_EXTKEY.'/flexform_ds.xml');
+
+if (TYPO3_MODE == 'BE') {
+	$TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']['tx_jf360shots_pi1_wizicon'] = t3lib_extMgm::extPath($_EXTKEY).'pi1/class.tx_jf360shots_pi1_wizicon.php';
+}
+
+// require_once(t3lib_extMgm::extPath($_EXTKEY).'lib/class.tx_jf360shots_itemsProcFunc.php');
+?>
---
+++ res/tx_jf360shots_pi1.js
@@ -0,0 +1,9 @@
+
+<!-- ###TEMPLATE_JS### begin -->
+jQuery(document).ready(function() {
+	var options = jQuery.queryParams(###OPTIONS###);
+	jQuery(function() {
+		jQuery('####KEY###').gestalt(options);
+	});
+});
+<!-- ###TEMPLATE_JS### end -->
